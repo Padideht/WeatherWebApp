7ba192137b1de53c9284e088a7dfb987bfee1950
Padideht <tayebizadeh.padideh@gmail.com>
2024-05-30 22:34:08 +0200

    Add weather API and showing city and temperature

--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
-function searchingCity(event) {
+function showWeatherInfo(response) {
+  let tempElement = document.querySelector("#temperature");
+  tempElement.innerHTML = Math.round(response.data.temperature.current);
+  let shownCity = document.querySelector("#city");
+  shownCity.innerHTML = response.data.city;
+}
+
+function searchingEnteredCity(city) {
+  let ApiKey = "b4b90t01e78a6d3ae3ea0adb94efof84";
+  let ApiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${ApiKey}`;
+  axios.get(ApiUrl).then(showWeatherInfo);
+}
+
+function handleSearching(event) {
   event.preventDefault();
   let searchedCity = document.querySelector("#input-city");
-  let shownCity = document.querySelector("#city");
-  shownCity.innerHTML = searchedCity.value.tolowerCase();
+
+  searchingEnteredCity(searchedCity.value);
 }
 
 let searchingForm = document.querySelector("#search-form");
-searchingForm.addEventListener("submit", searchingCity);
+searchingForm.addEventListener("submit", handleSearching);
